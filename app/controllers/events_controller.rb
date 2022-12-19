1bf37de1b685c95895a0fa18401d27921182ae86
fmikolic <mikolicfran@gmail.com>
2022-12-19 22:39:53 +0100

    Added list of events on index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,6 +15,14 @@ class EventsController < ApplicationController
 
     end
 
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def index
+        @events = Event.all
+    end
+
     private
 
     def event_params
